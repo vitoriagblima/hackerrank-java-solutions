ce0f5f445b2dde506508972a77b6c88c5775ebea
Vitória Lima <165972453+vitoriagblima@users.noreply.github.com>
2026-06-13 22:55:05 +0000

    feat: add java loops I solution

---
+++ src/introduction/LoopsI.java
@@ -0,0 +1,17 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class LoopsI{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println(N + " x " + i + " = " + (N * i));
+        }
+
+        sc.close();
+    }
+}
